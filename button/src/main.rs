ac5705536cb9de314f69c51b5d37a2ea118f3493
Grady Flanary <grady.flanary@gmail.com>
2022-06-24 22:31:47 -0500

    Now with COMPLETE FizzBuzz compatability!

--- button/src/main.rs
+++ button/src/main.rs
@@ -1,13 +1,16 @@
 use yew::prelude::*;
 
 struct Model {
-    value: i64
+    value: i64,
+    fizz_state: String
+        
 }
 
 #[function_component(App)]
 fn app() -> Html {
     let state = use_state(|| Model {
-        value: 0
+        value: 0,
+        fizz_state: String::from("0")
     });
 
     let onclick = {
@@ -15,7 +18,15 @@ fn app() -> Html {
 
         Callback::from(move |_| {
             state.set(Model {
-                value: state.value + 1
+                value: state.value + 1,
+                fizz_state: match (state.value % 3, state.value % 5, state.value){
+                    (_,_,0) => state.value.to_string(),
+                    (0,0,_) => String::from("FizzBuzz"),
+                    (0,_,_) => String::from("Fizz"),
+                    (_,0,_) => String::from("Buzz"),
+                    _ => state.value.to_string()
+
+                }
             }
             )
         }
@@ -25,7 +36,7 @@ fn app() -> Html {
     html! {
         <div>
             <button {onclick}>{" +1 "}</button>
-            <p>{ state.value }</p>
+            <p>{ &*state.fizz_state }</p>
         </div>
     }
     }
